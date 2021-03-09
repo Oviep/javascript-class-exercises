e9eb1629dd901410bdf5688cca1d2b8ffbff36fd
Omonigho Ovie Oddiri <ovie52009@yahoo.com>
2021-03-09 16:43:13 +0100

    code for exercises

---
+++ sample.js
@@ -0,0 +1,58 @@
+
+
+//Exercises
+//1.
+// let x = parseInt( prompt("Please enter number"));
+// let y = parseInt( prompt("Please enter number")); 
+// let sum = x + y;
+
+
+// function checkNumbers(x, y) {
+ 
+  
+//     if(sum === 50 || x === 50 || y === 50) {
+//        return true;
+        
+//     } else {
+//         return  false;
+//     }
+
+//     }
+
+// let check = checkNumbers(x, y);
+// console.log(check)
+
+//2.
+// let a = parseInt( prompt("Please enter number"));
+// let b = parseInt( prompt("Please enter number")); 
+
+// function checkNumbers2 (a, b) {
+//     if(a > 0 && b < 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// console.log(checkNumbers2(a, b))
+
+
+//3.
+let A;
+let s;
+let a = 5;
+let b = 6;
+let c = 7
+
+function scaleneTriangle() {
+    s = (5 + 6 + 7)/2
+    A = Math.sqrt(s*((s - a)*(s - b)*(s -c)));
+    return A;
+}
+
+console.log(scaleneTriangle());
+
+//4.
+
+
+
